923ac108ad5ca9e7ad88662994bd7f0838e554d7
speedwordpress <dani@speedword.press>
2017-08-23 18:59:09 +0200

    They Working

---
+++ modules/speedwp-add-extras-editor.php
@@ -0,0 +1,32 @@
+﻿<?php
+/*
+Module Name: WordPress Editor erweitern
+Module URI: Hilfe Link
+Description: WordPress Editor erweitern.
+
+
+f [Backend]
+Author: Daniel Bieli
+Author URI: https://speedword.press
+*/
+
+
+/* Sicherheitsabfrage */
+if ( !class_exists('Toolbox') ) {
+	die();
+}
+
+
+/* WordPress Editor erweitern */
+
+function add_more_buttons($buttons) {
+ $buttons[] = 'del';
+ $buttons[] = 'sub';
+ $buttons[] = 'sup';
+ $buttons[] = 'fontselect';
+ $buttons[] = 'fontsizeselect';
+ $buttons[] = 'cleanup';
+ $buttons[] = 'styleselect';
+ return $buttons;
+}
+add_filter("mce_buttons_3", "add_more_buttons");
---
+++ modules/speedwp-rm-search.php
@@ -0,0 +1,33 @@
+﻿<?php
+/*
+Module Name: WordPress Suche deaktivieren 
+Module URI: Hilfe Link
+Description: WordPress Suche deaktivieren [Backend]
+Author: Daniel Bieli
+Author URI: https://speedword.press
+*/
+
+
+/* Sicherheitsabfrage */
+if ( !class_exists('Toolbox') ) {
+	die();
+}
+
+
+/* WordPress Suche deaktivieren */
+
+function ah_filter_query( $query, $error = true ) {
+
+if ( is_search() ) {
+$query->is_search = false;
+$query->query_vars[s] = false;
+$query->query[s] = false;
+
+// to error
+if ( $error == true )
+$query->is_404 = true;
+}
+}
+
+add_action( 'parse_query', 'ah_filter_query' );
+add_filter( 'get_search_form', create_function( '$a', "return null;" ) );
