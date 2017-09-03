6ef2d7e8323eb5c02e8907ca9106674f218008f9
speedwordpress <dani@speedword.press>
2017-09-03 06:18:27 +0200

    neu

---
+++ modules/speedwp-rm-heartbeat-in-dashboard.php
@@ -0,0 +1,24 @@
+﻿<?php
+/*
+Module Name: WordPress Heartbeat API nur im Dashboard deaktivieren
+Module URI: Hilfe Link
+Description: WordPress Heartbeat API nur im Dashboard deaktivieren [Backend]
+Author: Daniel Bieli
+Author URI: https://speedword.press
+*/
+
+
+/* Sicherheitsabfrage */
+if ( !class_exists('Toolbox') ) {
+	die();
+}
+
+
+/* WordPress Heartbeat API nur im Dashboard deaktivieren */
+
+add_action('init', 'stop_heartbeat', 1);
+function stop_heartbeat()
+	{
+	global $pagenow;
+	if ($pagenow == 'index.php') wp_deregister_script('heartbeat');
+	}
--- modules/speedwp-rm-posts-via-mail.php
+++ modules/speedwp-rm-posts-via-mail.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Module Name: Blog Posts via Mail deaktivieren
+Module Name: Prism Syntax Highlighter
 Module URI: Hilfe Link
-Description: Blog Posts via Mail deaktivieren [Backend]
+Description: Prism Syntax Highlighter - <p>Beispiel: <strong>&lt;pre&gt;&lt;code class="language-php"&gt;Dein Code&lt;/code&gt;&lt;/pre&gt;</strong></p> [Frontend]
 Author: Daniel Bieli
 Author URI: https://speedword.press
 */
@@ -14,6 +14,53 @@ if ( !class_exists('Toolbox') ) {
 }
 
 
-/** Post via Mail deaktivieren */
+/* Prism Syntax Highlighter */
 
-add_filter('enable_post_by_email_configuration', '__return_false');
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+/**
+ * Load all JavaScript to header
+ *
+ * This function is attached to the 'wp_enqueue_scripts' action hook.
+ *
+ * @uses	is_admin()
+ * @uses	is_singular()
+ * @uses	wp_enqueue_script()
+ * @uses	plugins_url()
+ *
+ * @since 1.0.0
+ */
+if ( ! function_exists('ah_add_prism') ) {
+// Make function pluggable    
+function ah_add_prism() {
+		if ( is_single() || is_page() ) {
+			wp_enqueue_script( 'prism-js', plugins_url( 'js/prism.js', __FILE__ ), '', '', false );
+			wp_enqueue_style( 'prism-css', plugins_url( 'css/prism.css', __FILE__ ) );
+		}
+    }
+}
+add_action( 'wp_enqueue_scripts', 'ah_add_prism' );
+
+
+
+/**
+ * We provide a small user guide for the Prism Highlighter under the Help button
+ * @author Andreas Hecht
+ */
+function tb_contextual_help_prism( $contextual_help, $screen_id, $screen ) {
+
+if ( 'post' == $screen->id ) {
+
+$screen->add_help_tab( array(
+'id' => 'syntax_highlighter',
+'title' => __('Prism Highlighter'),
+'content' => '<h2>How to use the Prism Highlighter</h2>
+<h3>DE: Möglichkeit Nummer 1:</h3>
+<p>Du hast den Code von einem <a href="https://gist.github.com/" target="_blank">Gist bei Github</a> bekommen. Dann kannst Du den Code direkt in den Visuellen Editor einfügen, WordPress umschließt den Code automatisch mit dem &lt;pre&gt; Element. Gehe im Anschluss daran in die Textansicht und füge dort hinter dem &lt;pre&gt; Element ein &lt;code class="language-xxx"&gt; hinzu. XXX steht für den Code, den es zu highlighten gilt. Unterstützt werden <strong>markup (html), css, php und javascript</strong>. Vergiss nicht, dass vor dem schließenden &lt;/pre&gt; ebenfalls ein schließendes &lt;/code&gt; gehört. Ist dies erledigt, kannst Du bereits wieder in die visuelle Ansicht des Editors wechseln.</p><p>Beispiel: <strong>&lt;pre&gt;&lt;code class="language-php"&gt;Dein Code&lt;/code&gt;&lt;/pre&gt;</strong></p>
+<h3>DE: Möglichkeit Nummer 2:</h3>
+<p>Dein Code stammt aus Deinem HTML-Editor (wie z.B. Brackets, Atom, Dreamweaver). Dann klicke in der visuellen Ansicht zuerst auf »Vorvormatiert« und füge den Code dann ein. Wechsle im Anschluss daran dann in die Textansicht und ergänze dort wie bei Möglichkeit eins das Code-Element mit der Language-Klasse.</p><p>Beispiel: <strong>&lt;pre&gt;&lt;code class="language-php"&gt;Dein Code&lt;/code&gt;&lt;/pre&gt;</strong></p>',
+        ));     
+    }
+}
+add_action( 'contextual_help', 'tb_contextual_help_prism', 10, 3 );
+   
